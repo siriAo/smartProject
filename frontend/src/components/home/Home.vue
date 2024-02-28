29cb42649565bacebe119a2ad3f7563c709272eb
siriAo <1959695645@qq.com>
2024-02-29 03:12:36 +0800

    UserManagement Home

--- frontend/src/components/home/Home.vue
+++ frontend/src/components/home/Home.vue
@@ -1,68 +1,101 @@
 <template>
   <el-container>
-    <el-aside width="auto">
-      <el-radio-group v-model="isCollapse" style="margin-bottom: 20px">
-        <el-radio-button :label="false">expand</el-radio-button>
-        <el-radio-button :label="true">collapse</el-radio-button>
-      </el-radio-group>
-      <el-menu
-          default-active="2"
-          class="el-menu-vertical-demo"
-          :collapse="isCollapse"
-          @open="handleOpen"
-          @close="handleClose"
-      >
-        <el-menu-item index="6">
-          <el-icon><Histogram /></el-icon>
-          <template #title>数据总览</template>
-        </el-menu-item>
-        <el-sub-menu index="1">
-          <template #title>
-            <el-icon><Document /></el-icon>
-            <span>数据汇总</span>
-          </template>
-          <el-menu-item-group>
-            <template #title><span>Group One</span></template>
-            <el-menu-item index="1-1">item one</el-menu-item>
-            <el-menu-item index="1-2">item two</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="Group Two">
-            <el-menu-item index="1-3">item three</el-menu-item>
-          </el-menu-item-group>
-          <el-sub-menu index="1-4">
-            <template #title><span>item four</span></template>
-            <el-menu-item index="1-4-1">item one</el-menu-item>
-          </el-sub-menu>
-        </el-sub-menu>
-        <el-menu-item index="2">
-          <el-icon><UserFilled/></el-icon>
-          <template #title>用户管理</template>
-        </el-menu-item>
-        <el-menu-item index="3" disabled>
-          <el-icon>
-            <document/>
-          </el-icon>
-          <template #title>管理员操作</template>
-        </el-menu-item>
-        <el-menu-item index="4">
-          <el-icon>
-            <setting/>
-          </el-icon>
-          <template #title>设置</template>
-        </el-menu-item>
-      </el-menu>
-    </el-aside>
+
+    <el-header style="height: auto">
+      <el-row class="block-col-2" justify="end">
+        <el-col :span="4">
+          <el-dropdown size="large">
+        <span class="el-dropdown-link" style="font-size: larger;text-align: right ">
+          菠萝偷吃豆腐<el-icon class="el-icon--right"><arrow-down/></el-icon>
+        </span>
+            <template #dropdown>
+              <el-dropdown-menu>
+                <el-dropdown-item :icon="CirclePlusFilled">我的信息</el-dropdown-item>
+                <el-dropdown-item :icon="CircleCheck">登出</el-dropdown-item>
+              </el-dropdown-menu>
+            </template>
+          </el-dropdown>
+        </el-col>
+      </el-row>
+    </el-header>
 
     <el-container style="height: auto">
-      <el-header>用户信息</el-header>
-      <el-main>内容展示
+      <el-aside width="auto">
+        <el-radio-group v-model="isCollapse" style="margin-bottom: 20px">
+          <el-radio-button :label="false">expand</el-radio-button>
+          <el-radio-button :label="true">collapse</el-radio-button>
+        </el-radio-group>
+        <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            :collapse="isCollapse"
+            @open="handleOpen"
+            @close="handleClose"
+        >
+          <el-menu-item index="6">
+            <el-icon>
+              <Histogram/>
+            </el-icon>
+            <template #title>数据总览</template>
+          </el-menu-item>
+          <el-sub-menu index="1">
+            <template #title>
+              <el-icon>
+                <Document/>
+              </el-icon>
+              <span>数据汇总</span>
+            </template>
+            <el-menu-item-group>
+              <template #title><span>Group One</span></template>
+              <el-menu-item index="1-1">item one</el-menu-item>
+              <el-menu-item index="1-2">item two</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="Group Two">
+              <el-menu-item index="1-3">item three</el-menu-item>
+            </el-menu-item-group>
+            <el-sub-menu index="1-4">
+              <template #title><span>item four</span></template>
+              <el-menu-item index="1-4-1">item one</el-menu-item>
+            </el-sub-menu>
+          </el-sub-menu>
+          <el-menu-item index="2">
+            <el-icon>
+              <UserFilled/>
+            </el-icon>
+            <template #title>用户管理</template>
+          </el-menu-item>
+          <el-menu-item index="3" disabled>
+            <el-icon>
+              <document/>
+            </el-icon>
+            <template #title>管理员操作</template>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <el-icon>
+              <setting/>
+            </el-icon>
+            <template #title>设置</template>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-main>
         <router-view/>
       </el-main>
     </el-container>
+
   </el-container>
+
 </template>
 
 <script lang="ts" setup>
+import {
+  ArrowDown,
+  Check,
+  CircleCheck,
+  CirclePlus,
+  CirclePlusFilled,
+  Plus,
+} from '@element-plus/icons-vue'
 import {ref} from 'vue'
 import {
   Document,
@@ -84,5 +117,17 @@ const handleClose = (key: string, keyPath: string[]) => {
 .el-menu-vertical-demo:not(.el-menu--collapse) {
   width: 200px;
   min-height: 400px;
+}
+
+.block-col-2 .demonstration {
+  display: block;
+  color: var(--el-text-color-secondary);
+  font-size: 14px;
+  margin-bottom: 20px;
+}
+
+.block-col-2 .el-dropdown-link {
+  display: flex;
+  align-items: center;
 }
 </style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,16 +1,18 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import Home from '@/components/home/Home.vue'
 import User from "@/components/user/User.vue";
+import UserManagement from "@/components/user/UserManagement.vue";
 import Login from "@/components/login/Login.vue";
 import Doc from "@/components/doc/Doc.vue";
 import Demo from "@/components/Demo.vue";
+
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
-            redirect: '/demo'
+            redirect: '/doc'
         },
         {
             path: '/demo',
@@ -33,20 +35,17 @@ const router = createRouter({
                     name: "user"
                 },
                 {
+                    path: 'userManagement',
+                    component: UserManagement,
+                    name: "userManagement"
+                },
+                {
                     path: 'doc',
                     component: Doc,
                     name: "doc"
                 }
             ]
-        },
-        // {
-        // path: '/about',
-        // name: 'about',
-        // route level code-splitting
-        // this generates a separate chunk (About.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        // component: () => import('../views/AboutView.vue')
-        // }
+        }
     ]
 })
 
